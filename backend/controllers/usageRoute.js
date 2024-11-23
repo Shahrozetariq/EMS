8d080804c8f722472bc27b71ec7fc90532544085
Shahroze Tariq <shazzt55@gmail.com>
2024-11-23 17:02:56 +0500

    Multiple

--- backend/controllers/usageRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getMonthlyUsageByCompany } = require('./monthlyUsage');
-
-// Route to get monthly usage by company ID
-router.get('/companies/:companyId/monthly-usage', getMonthlyUsageByCompany);
-
-module.exports = router;
